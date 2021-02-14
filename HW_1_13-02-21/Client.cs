316818661d93177a41a4dd6f596baec0c53c2cb1
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-02-14 19:37:23 +0500

    Добавьте файлы проекта.

---
+++ HW_1_13-02-21/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_1_13_02_21
+{
+    class Client
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public decimal Balance { get; set; }
+        public string PassportNO { get; set; }
+        public Client(Guid id, string firstName, string lastName, string middleName, decimal balance, string passportNO)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            MiddleName = middleName;
+            Balance = balance;
+            PassportNO = passportNO;
+        }
+    }
+}
