45917068d47a3ac937b7f223ff4c074c70aab347
Huseyn <huseyn19.burhonov1999@gmail.com>
2021-02-14 21:40:18 +0500

    Added switch

--- HW_1_13-02-21/Program.cs
+++ HW_1_13-02-21/Program.cs
@@ -8,11 +8,32 @@ namespace HW_1_13_02_21
     {
         static void Main(string[] args)
         {
-
-
-
-
-
+            while (true)
+            {
+                Console.WriteLine(@"Выбирите комманду:
+                        1. Вставить ---> 1
+                        2. Выбрать  ---> 2
+                        3. Изменить ---> 3
+                        4. Удалять  ---> 4");
+                switch (int.Parse(Console.ReadLine()))
+                {
+                    case 1:
+                        Client.Insert();
+                        break;
+                    case 2:
+                        Client.Select();
+                        break;
+                    case 3:
+                        Client.UpdateById();
+                        break;
+                    case 4:
+                        Client.DeleteById();
+                        break;
+                    default:
+                        return;
+                        break;
+                }
+            }
             //Client.Insert();
             //Client.Select();
             //Client.Update();
